0444450321e722f61454184df775f8a353b33639
wark99 <vaidamark99@gmail.com>
2021-03-02 17:36:02 +0200

    Edited: LiveVideoFragment.kt, AndroidManifest.xml

--- app/src/main/java/com/example/allamvizsga_2020_21/main/LiveVideoFragment.kt
+++ app/src/main/java/com/example/allamvizsga_2020_21/main/LiveVideoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.allamvizsga_2020_21.main
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,30 +13,49 @@ import com.example.allamvizsga_2020_21.R
 
 class LiveVideoFragment : Fragment() {
 
-    private lateinit var videoPlayer: VideoView
+    private lateinit var videoView: VideoView
     private lateinit var mediaController: MediaController
+    private val onlineSource =
+        Uri.parse("https://cdn.api.video/vod/vi3pfGrhpRkGPTJkZOhdHmJ6/mp4/720/source.mp4")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_video, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_live_video, container, false)
 
-    override fun onResume() {
-        super.onResume()
-
-        val currentActivity = requireActivity()
-
-        videoPlayer = currentActivity.findViewById(R.id.liveVideo)
+        videoView = view.findViewById(R.id.liveVideo)
         mediaController = MediaController(requireContext())
-        mediaController.setAnchorView(videoPlayer)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
 
-        val onlineUri = Uri.parse("https://cdn.api.video/vod/vi3m7zwazDTZ6IFdnlQ1xHTJ/mp4/720/source.mp4")
-        videoPlayer.setMediaController(mediaController)
-        videoPlayer.setVideoURI(onlineUri)
-        videoPlayer.requestFocus()
-        videoPlayer.start()
+        return view
     }
+
+    private fun initializePlayer() {
+        videoView.setVideoURI(onlineSource)
+        videoView.start()
+    }
+
+    private fun releasePlayer() {
+        videoView.stopPlayback()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        videoView.pause()
+    }
+
+
 }
